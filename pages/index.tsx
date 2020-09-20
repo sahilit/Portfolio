a2c76e1b0f8fa3d03d14c96abdc08af0395d2b33
Sahil Singh <sahilsinghmca@gmail.com>
2020-09-21 02:57:03 +0530

    removed vulnerable third party links

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,11 +25,11 @@ export default function Home() {
 				<link rel="icon" href="images/logo.jpg" />
 				{/* Latest compiled and minified CSS */}
 				{/* @ts-ignore */}
-				<link rel="preload" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" as="style" media="all" onLoad="this.onload=null;this.rel='stylesheet'" />
+				<link rel="preload" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" as="style" media="all" onLoad="this.onload=null;this.rel='stylesheet'" />
 				{/* jQuery library */}
-				<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+				<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 				{/* Latest compiled JavaScript */}
-				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 				{/* Font awesome */}
 				{/* @ts-ignore */}
 				<link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" as="style" media="all" onLoad="this.onload=null;this.rel='stylesheet'" />

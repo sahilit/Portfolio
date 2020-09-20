3e0b5bba24a6e6c75f01e0c208b86f0f6b77c4cc
Sahil Singh <sahilsinghmca@gmail.com>
2020-09-21 02:03:09 +0530

    font display fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,7 @@ export default function Home() {
 				<link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" as="style" media="all" onLoad="this.onload=null;this.rel='stylesheet'" />
 				{/* Google font Roboto */}
 				{/* @ts-ignore */}
-				<link rel="preload" href='https://fonts.googleapis.com/css?family=Roboto' as="style" media="all" onLoad="this.onload=null;this.rel='stylesheet'" />
+				<link rel="preload" href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" as="style" media="all" onLoad="this.onload=null;this.rel='stylesheet'" />
 				<script type="text/javascript" src="/main.js"></script>
 			</Head>
 
